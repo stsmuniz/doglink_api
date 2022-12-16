e4b266b555002c4d7cb842d6958e3da1397e811b
Salustiano Muniz <salustiano.muniz.ext@infracommerce.com.br>
2022-12-16 17:58:21 -0300

    test database connection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/db-test', function () {
+    if(DB::connection()->getDatabaseName())
+    {
+        echo "Connected to database ".DB::connection()->getDatabaseName();
+    }
 });
 
 Route::get('/profile/{slug}', [PageController::class, 'show']);
