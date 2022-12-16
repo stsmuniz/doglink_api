135862c98bbd08a519f961396fc1c7a148259239
Salustiano Muniz <salustiano.muniz.ext@infracommerce.com.br>
2022-12-16 17:16:53 -0300

    config and test database connection

--- public/index.php
+++ public/index.php
@@ -48,6 +48,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
+if(DB::connection()->getDatabaseName())
+{
+    echo "Connected to database ".DB::connection()->getDatabaseName();
+}
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
