f9c0458f80935a13a0e39a2567af56af4f1052d8
Salustiano Muniz <salustiano.muniz.ext@infracommerce.com.br>
2022-12-17 13:36:43 -0300

    fix: colors column size on pages table

--- .platform/hooks/postdeploy/optimize_laravel.sh
+++ .platform/hooks/postdeploy/optimize_laravel.sh
@@ -9,4 +9,4 @@ php artisan route:cache
 
 php artisan view:cache
 
-php artisan migrate --force
+php artisan migrate:refresh --force
--- database/migrations/2022_11_07_032621_create_pages_table.php
+++ database/migrations/2022_11_07_032621_create_pages_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
-            $table->string('primary_color', 6)->default('ffffff');
-            $table->string('secondary_color', 6)->default('000000');
+            $table->string('primary_color', 7)->default('ffffff');
+            $table->string('secondary_color', 7)->default('000000');
             $table->string('custom_url', 20);
             $table->string('title', 20)->nullable();
             $table->string('tagline', 80)->nullable();
             $table->string('theme')->default('default');
             $table->foreignId('user_id')->constrained();
-            $table->string('background_color', 6)->default('ffffff');
-            $table->string('text_color', 6)->default('000000');
+            $table->string('background_color', 7)->default('ffffff');
+            $table->string('text_color', 7)->default('000000');
             $table->timestamps();
         });
     }
